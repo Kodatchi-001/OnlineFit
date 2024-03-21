b70bdd2724ff1128efcdbce5e1e20e60a99d001a
Ahmed hariri <ahmedhariri58@gmail.com>
2024-03-21 17:13:19 +0000

    up date javascript

--- programme.js
+++ programme.js
@@ -6,68 +6,70 @@ var img_5 = document.getElementById('img-5');
 var img_6 = document.getElementById('img-6');
 
 var card_img = document.getElementById('card-img');
-var img = document.getElementById('change');
+var img_haja = document.getElementById('change');
 
 var tittle = document.getElementById('tittle');
 var para = document.getElementById('para');
 
 img_1.onclick = function name(params) {
     img.style.marginLeft = '0vw'
-    img.src = './Assets/basquete.png'
+    img_haja.src = './Assets/basquete.png'
+    img_haja.style.width = '25vw'
+    img_haja.style.marginLeft = '0vw'
     tittle.innerHTML = 'Basket ball'
     tittle.style.fontSize = '10vw'
-    img.style.marginBottom = '0vh'
-    img.style.width = '25vw'
     para.innerHTML = ' Basketball is a fast-paced sport played between two teams of five players each, aiming to score points by shooting the ball through the opponents hoop.'
 }
 
 img_2.onclick = function name(params) {
     img.style.marginLeft = '0vw'
-    img.src = './Assets/fitness.png'
+    img_haja.src = './Assets/fitness.png'
+    img_haja.style.width = '25vw'
+    img_haja.style.marginLeft = '0vw'
     tittle.innerHTML = 'Fitness'
     tittle.style.fontSize = '10vw'
-    img.style.marginBottom = '0vh'
-    img.style.width = '25vw'
     para.innerHTML = 'Fitness is a holistic approach to physical well-being that encompasses exercise, nutrition, and overall health. It involves engaging in regular physical activity to improve cardiovascular health, strength, flexibility, and endurance.'
 }
 
 img_3.onclick = function name(params) {
     img.style.marginLeft = '0vw'
-    img.src = './Assets/tenis.png'
+    img_haja.src = './Assets/tenis.png'
+    img_haja.style.width = '25vw'
+    img_haja.style.marginLeft = '0vw'
+    img_haja.style.marginLeft = '0vw'
     tittle.innerHTML = 'Tennis'
     tittle.style.fontSize = '10vw'
-    img.style.marginBottom = '0vh'
-    img.style.width = '25vw'
     para.innerHTML = 'Tennis is a racket sport played individually or in pairs, where players use rackets to hit a ball over a net into the opponents court. The objective is to score points by making it difficult .'
 }
 
 img_4.onclick = function name(params) {
     img.style.marginLeft = '0vw'
-    img.src = './Assets/rigbi.png'
+    img_haja.src = './Assets/rigbi.png'
+    img_haja.style.width = '25vw'
+    img_haja.style.marginLeft = '0vw'
     tittle.innerHTML = 'Rugby'
     tittle.style.fontSize = '10vw'
-    img.style.marginBottom = '0vh'
-    img.style.width = '25vw'
     para.innerHTML = 'Rugby is a full-contact team sport played with an oval-shaped ball. It is typically played by two teams of 15 players each, although variations such as rugby sevens involve fewer players. The objective of the game is to score points by carrying'
 }
 
 img_5.onclick = function name(params) {
     img.style.marginLeft = '0vw'
-    img.src = './Assets/footballe.png'
+    img_haja.src = './Assets/footballe.png'
+    img_haja.style.width = '18vw'
+    img_haja.style.marginLeft = '0vw'
     tittle.innerHTML = 'Football'
     tittle.style.fontSize = '9vw'
-    img.style.marginBottom = '-18vh'
-    img.style.width = '25vw'
     para.innerHTML = 'Football, also known as soccer in some regions, is a team sport played between two teams of eleven players each. The objective of the game is to score goals by kicking a ball into the opponents goal'
 }
 
 img_6.onclick = function name(params) {
     img.style.marginLeft = '0vw'
-    img.src = './Assets/moto.png'
+    img_haja.src = './Assets/moto.png'
     tittle.innerHTML = 'Motor Racing'
     tittle.style.fontSize = '10vw'
+    img_haja.style.marginLeft = '0vw'
     img.style.marginBottom = '0vh'
-    img.style.width = '25vw'
+    img.style.width = '30vw'
     para.innerHTML = 'Motor racing, often referred to simply as "racing," is a competitive sport involving vehicles propelled by internal combustion engines or electric motors. Participants, known as drivers'
 }
 
@@ -89,7 +91,7 @@ var haja = document.getElementById("content")
 var haja_2 = document.getElementById("plus-num")
 var haja_3 = document.getElementById("moin-num")
 
-var para = document.getElementById('para');
+var para_2 = document.getElementById('para-2');
 var info_1 = document.getElementById('inf-1');
 var info_2 = document.getElementById('inf-2');
 
@@ -105,6 +107,7 @@ haja_2.onclick = function () {
     if (haja.textContent == 1) {
         info_1.innerHTML = 'Thomas Martin'
         info_2.innerHTML = 'Freelancer'
+        para_2.innerHTML = 'I find online fitness sites offer great flexibility for working out at any time that suits my busy schedule. Some even have specific programs for busy individuals like me, which is really convenient.'
         najma_1.style.color = 'orange'
         najma_2.style.color = 'orange'
         najma_3.style.color = 'orange'
@@ -114,6 +117,7 @@ haja_2.onclick = function () {
     else if (haja.textContent == 2) {
         info_1.innerHTML = 'Sophie Leclerc'
         info_2.innerHTML = 'Infirmière'
+        para_2.innerHTML = 'I appreciate the variety of workout options available on online fitness sites. It allows me to diversify my exercise sessions and discover new activities without having to subscribe to multiple different memberships.'
         najma_1.style.color = 'orange'
         najma_2.style.color = 'orange'
         najma_3.style.color = 'black'
@@ -123,6 +127,7 @@ haja_2.onclick = function () {
     else if (haja.textContent == 3) {
         info_1.innerHTML = 'Émilie Rousseau'
         info_2.innerHTML = 'Chef cuisinier'
+        para_2.innerHTML = 'I think online fitness sites are a great solution for staying fit without having to leave the house. However, I still prefer the social aspect of in-person classes. Sometimes, I feel less motivated when working out alone at home'
         najma_1.style.color = 'orange'
         najma_2.style.color = 'orange'
         najma_3.style.color = 'orange'
@@ -132,6 +137,7 @@ haja_2.onclick = function () {
     else if (haja.textContent == 4) {
         info_1.innerHTML = 'Julien Moreau '
         info_2.innerHTML = 'Journaliste'
+        para_2.innerHTML = 'I m a big fan of online fitness sites because they allow me to customize my workouts based on my goals and abilities. Plus I can save on gym fees and work out comfortably at home'
         najma_1.style.color = 'orange'
         najma_2.style.color = 'orange'
         najma_3.style.color = 'orange'
@@ -141,6 +147,7 @@ haja_2.onclick = function () {
     else if (haja.textContent == 5) {
         info_1.innerHTML = 'Laura Dubois'
         info_2.innerHTML = 'Psychologue'
+        para_2.innerHTML = 'Online fitness sites are convenient for me because they often offer high-quality workout videos based on scientific evidence. I feel confident following these programs, knowing they are developed by qualified professionals.'
         najma_1.style.color = 'orange'
         najma_2.style.color = 'black'
         najma_3.style.color = 'black'
@@ -150,6 +157,7 @@ haja_2.onclick = function () {
     else if (haja.textContent == 6) {
         info_1.innerHTML = 'Élodie Bertrand'
         info_2.innerHTML = 'Designer graphique'
+        para_2.innerHTML = 'While I occasionally use online fitness sites for quick at-home workouts, I prefer in-person classes as I enjoy the interaction with the instructor and fellow participants. It motivates me more to push myself.'
         najma_1.style.color = 'orange'
         najma_2.style.color = 'orange'
         najma_3.style.color = 'orange'
@@ -159,6 +167,7 @@ haja_2.onclick = function () {
     else if (haja.textContent == 7) {
         info_1.innerHTML = 'Nicolas Lefebvre'
         info_2.innerHTML = 'Avocat'
+        para_2.innerHTML = 'Online fitness sites provide me with the convenience to stay active amidst my demanding legal career. While I prefer the structure of in-person classes, the flexibility of online platforms allows me to maintain a consistent workout routine.'
         najma_1.style.color = 'orange'
         najma_2.style.color = 'orange'
         najma_3.style.color = 'orange'
@@ -168,6 +177,7 @@ haja_2.onclick = function () {
     else if (haja.textContent <= 8) {
         info_1.innerHTML = 'Léa Bergeron'
         info_2.innerHTML = 'Développeur web'
+        para_2.innerHTML = 'Online fitness sites are perfect for me as I can work out at any hour that suits me, even late at night after a long day of work. Plus, some sites offer specific programs for people who work in front of a computer all day'
         najma_1.style.color = 'orange'
         najma_2.style.color = 'orange'
         najma_3.style.color = 'orange'
@@ -184,6 +194,7 @@ haja_3.onclick = function () {
     if (haja.textContent <= 1) {
         info_1.innerHTML = 'Thomas Martin'
         info_2.innerHTML = 'Freelancer'
+        para_2.innerHTML = 'I find online fitness sites offer great flexibility for working out at any time that suits my busy schedule. Some even have specific programs for busy individuals like me, which is really convenient.'
         najma_1.style.color = 'orange'
         najma_2.style.color = 'orange'
         najma_3.style.color = 'orange'
@@ -194,6 +205,7 @@ haja_3.onclick = function () {
     else if (haja.textContent == 2) {
         info_1.innerHTML = 'Sophie Leclerc'
         info_2.innerHTML = 'Infirmière'
+        para_2.innerHTML = 'I appreciate the variety of workout options available on online fitness sites. It allows me to diversify my exercise sessions and discover new activities without having to subscribe to multiple different memberships.'
         najma_1.style.color = 'orange'
         najma_2.style.color = 'orange'
         najma_3.style.color = 'black'
@@ -203,6 +215,7 @@ haja_3.onclick = function () {
     else if (haja.textContent == 3) {
         info_1.innerHTML = 'Émilie Rousseau'
         info_2.innerHTML = 'Chef cuisinier'
+        para_2.innerHTML = 'I think online fitness sites are a great solution for staying fit without having to leave the house. However, I still prefer the social aspect of in-person classes. Sometimes, I feel less motivated when working out alone at home'
         najma_1.style.color = 'orange'
         najma_2.style.color = 'orange'
         najma_3.style.color = 'orange'
@@ -212,6 +225,7 @@ haja_3.onclick = function () {
     else if (haja.textContent == 4) {
         info_1.innerHTML = 'Julien Moreau '
         info_2.innerHTML = 'Journaliste'
+        para_2.innerHTML = 'I m a big fan of online fitness sites because they allow me to customize my workouts based on my goals and abilities. Plus I can save on gym fees and work out comfortably at home'
         najma_1.style.color = 'orange'
         najma_2.style.color = 'orange'
         najma_3.style.color = 'orange'
@@ -221,6 +235,7 @@ haja_3.onclick = function () {
     else if (haja.textContent == 5) {
         info_1.innerHTML = 'Laura Dubois'
         info_2.innerHTML = 'Psychologue'
+        para_2.innerHTML = 'Online fitness sites are convenient for me because they often offer high-quality workout videos based on scientific evidence. I feel confident following these programs, knowing they are developed by qualified professionals.'
         najma_1.style.color = 'orange'
         najma_2.style.color = 'black'
         najma_3.style.color = 'black'
@@ -230,6 +245,7 @@ haja_3.onclick = function () {
     else if (haja.textContent == 6) {
         info_1.innerHTML = 'Élodie Bertrand'
         info_2.innerHTML = 'Designer graphique'
+        para_2.innerHTML = 'While I occasionally use online fitness sites for quick at-home workouts, I prefer in-person classes as I enjoy the interaction with the instructor and fellow participants. It motivates me more to push myself.'
         najma_1.style.color = 'orange'
         najma_2.style.color = 'orange'
         najma_3.style.color = 'orange'
@@ -239,6 +255,7 @@ haja_3.onclick = function () {
     else if (haja.textContent == 7) {
         info_1.innerHTML = 'Nicolas Lefebvre'
         info_2.innerHTML = 'Avocat'
+        para_2.innerHTML = 'Online fitness sites provide me with the convenience to stay active amidst my demanding legal career. While I prefer the structure of in-person classes, the flexibility of online platforms allows me to maintain a consistent workout routine.'
         najma_1.style.color = 'orange'
         najma_2.style.color = 'orange'
         najma_3.style.color = 'orange'
@@ -248,6 +265,7 @@ haja_3.onclick = function () {
     else if (haja.textContent == 8) {
         info_1.innerHTML = 'Léa Bergeron'
         info_2.innerHTML = 'Développeur web'
+        para_2.innerHTML = 'Online fitness sites are perfect for me as I can work out at any hour that suits me, even late at night after a long day of work. Plus, some sites offer specific programs for people who work in front of a computer all day'
         najma_1.style.color = 'orange'
         najma_2.style.color = 'orange'
         najma_3.style.color = 'orange'
@@ -271,16 +289,16 @@ var card_3 = document.getElementById('card-3');
 
 window.onscroll = function () {
     //page-1
-    if (scrollY >= 500) {
+    if (scrollY >= 600) {
         //Move-1
         scrol_3.style.transition = '2s'
         scrol_3.style.marginLeft = '0vw'
         //Move-2
-        card_1.style.transition = '2s'
+        card_1.style.transition = '1.5s'
         card_1.style.transform = 'scale(1)'
-        card_2.style.transition = '3s'
+        card_2.style.transition = '2s'
         card_2.style.transform = 'scale(1)'
-        card_3.style.transition = '4s'
+        card_3.style.transition = '2.5s'
         card_3.style.transform = 'scale(1)'
     }
     else if (scrollY <= 450) {
